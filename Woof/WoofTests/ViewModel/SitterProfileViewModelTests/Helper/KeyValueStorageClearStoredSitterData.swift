97afb4bead0e85b84a545d3dbfb1f64609e7e0d6
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-10-10 10:57:03 +0200

    Changing helper func for clearing current sitter keyvaluestorage

--- Woof/WoofTests/ViewModel/SitterProfileViewModelTests/Helper/KeyValueStorageClearStoredSitterData.swift
+++ Woof/WoofTests/ViewModel/SitterProfileViewModelTests/Helper/KeyValueStorageClearStoredSitterData.swift
@@ -1,7 +1,6 @@
 extension KeyValueStorage {
     /// Clears stored sitters data from storage.
     static func clearStoredSitterData() {
-        let keyValueStorage = KeyValueStorage(KeyValueStorage.Name.currentSitter)
-        keyValueStorage.deleteData(for: KeyValueStorage.Key.currentSitter)
+        KeyValueStorage(Self.Name.currentSitter).deleteData(for: Self.Key.currentSitter)
     }
 }
