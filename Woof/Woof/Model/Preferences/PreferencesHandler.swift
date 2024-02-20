8e9135227c9f3d63dabc2b7bc641e7e25cc7dd74
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-02-20 11:22:10 +0100

    Extending the functionality of PreferencesHandler to work with new properties

--- Woof/Woof/Model/Preferences/PreferencesHandler.swift
+++ Woof/Woof/Model/Preferences/PreferencesHandler.swift
@@ -24,12 +24,62 @@ enum PreferencesHandler {
     }
 
     /**
+     Saves the information whether the owner has logged in at least once.
+
+     - Parameters:
+        - isOwnerLoggedOnce: A boolean value indicating whether the owner has logged in at least once.
+
+     - Returns: A boolean value indicating whether the saving operation was successful.
+     */
+    @discardableResult static func setOwnerLoggedOnce(_ isOwnerLoggedOnce: Bool) -> Bool {
+        var preferences = loadPreferences() ?? Preferences()
+
+        preferences.isOwnerLoggedOnce = isOwnerLoggedOnce
+
+        return store(preferences)
+    }
+
+    /**
+     Saves the information whether the sitter has logged in at least once.
+
+     - Parameters:
+        - isSitterLoggedOnce: A boolean value indicating whether the sitter has logged in at least once.
+
+     - Returns: A boolean value indicating whether the saving operation was successful.
+     */
+    @discardableResult static func setSitterLoggedOnce(_ isSitterLoggedOnce: Bool) -> Bool {
+        var preferences = loadPreferences() ?? Preferences()
+
+        preferences.isSitterLoggedOnce = isSitterLoggedOnce
+
+        return store(preferences)
+    }
+
+    /**
      Gets the current user role in the app.
 
      - Returns: The current role for the user in the app session. If no role is selected, returns `.none`.
      */
     static func getUserRole() -> Role {
         loadPreferences()?.selectedRole ?? .none
+    }
+
+    /**
+     Gets the information whether the owner has logged in at least once.
+
+     - Returns: A boolean value indicating whether the owner has logged in at least once.
+     */
+    static func getOwnerLoggedOnce() -> Bool {
+        loadPreferences()?.isOwnerLoggedOnce ?? false
+    }
+
+    /**
+     Gets the information whether the sitter has logged in at least once.
+
+     - Returns: A boolean value indicating whether the sitter has logged in at least once.
+     */
+    static func getSitterLoggedOnce() -> Bool {
+        loadPreferences()?.isSitterLoggedOnce ?? false
     }
 
     // MARK: - Private interface
