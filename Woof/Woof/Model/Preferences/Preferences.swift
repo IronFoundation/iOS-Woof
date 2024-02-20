7cb7540929b75ecb632ac897eb5ca8793f69fba0
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-02-20 11:16:41 +0100

    Adding isOwnerLoggedOnce and isSitterLoggedOnce to the app preferences

--- Woof/Woof/Model/Preferences/Preferences.swift
+++ Woof/Woof/Model/Preferences/Preferences.swift
@@ -4,4 +4,10 @@ import Foundation
 struct Preferences: Codable {
     /// The selected role for the user, by default is `none`
     var selectedRole: Role = .none
+    
+    /// Indicates whether the owner has logged in at least once. Default is `false`.
+    var isOwnerLoggedOnce: Bool = false
+
+    /// Indicates whether the sitter has logged in at least once. Default is `false`.
+    var isSitterLoggedOnce: Bool = false
 }
