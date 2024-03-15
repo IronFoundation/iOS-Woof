2a4848e45915d2f4a20ceb2ddc9211e0da43b055
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-03-15 09:36:48 +0100

    Fixing typo and adding new CustomProgressView to SitterProfileView

--- Woof/Woof/View/ReusableViews/CustomProgressView.swift
+++ Woof/Woof/View/ReusableViews/CustomProgressView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-/// /// A view representing a custom circular progress indicator.
+/// A view representing a custom circular progress indicator.
 struct CustomProgressView: View {
     var body: some View {
         Color.white.opacity(AppStyle.UIElementConstant.opacityLevelForProgressViewBackground)
