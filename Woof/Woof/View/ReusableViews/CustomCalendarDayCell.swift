b4fe47a728529949bb617f6c502ff5ab1f1edc1f
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-04-29 12:09:04 +0200

    Adding functionality for DayCell

--- Woof/Woof/View/ReusableViews/CustomCalendarDayCell.swift
+++ Woof/Woof/View/ReusableViews/CustomCalendarDayCell.swift
@@ -1,11 +1,28 @@
 import SwiftUI
 
 struct CustomCalendarDayCell: View {
+    let date: Date
+    let isSelected: Bool
+    let onTap: () -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("\(Calendar.current.component(.day, from: date))")
+            .frame(width: cellSize, height: cellSize)
+            .background(isSelected ? Color.App.purpleLight : Color.clear)
+            .clipShape(Circle())
+            .onTapGesture {
+                onTap()
+            }
     }
+
+    // MARK: - Private interface
+
+    private let cellSize: CGFloat = 40
 }
 
 #Preview {
-    CustomCalendarDayCell()
+    HStack {
+        CustomCalendarDayCell(date: Date(), isSelected: true, onTap: {})
+        CustomCalendarDayCell(date: Date(), isSelected: false, onTap: {})
+    }
 }
