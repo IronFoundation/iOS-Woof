e4b28ff3039215d4a800d5d6fd9148e4b5142e57
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-04-29 12:09:38 +0200

    Adding functionality for Calendar to display current mont dates

--- Woof/Woof/View/ReusableViews/CustomCalendarView.swift
+++ Woof/Woof/View/ReusableViews/CustomCalendarView.swift
@@ -1,11 +1,38 @@
 import SwiftUI
 
 struct CustomCalendarView: View {
+    let selectedDates: Set<Date>
+    let onSelectDate: (Date) -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: Array(repeating: GridItem(), count: calendarColumnsCount), spacing: calendarGridSpacing) {
+            ForEach(getCurrentMonthDates(), id: \.self) { date in
+                CustomCalendarDayCell(date: date, isSelected: selectedDates.contains(date)) {
+                    onSelectDate(date)
+                }
+            }
+        }
+        .padding()
+    }
+
+    // MARK: - Private interface
+
+    private let calendarColumnsCount = 7
+    private let calendarGridSpacing: CGFloat = 10
+
+    private func getCurrentMonthDates() -> [Date] {
+        let calendar = Calendar.current
+        let currentDate = Date()
+
+        guard let range = calendar.range(of: .day, in: .month, for: currentDate),
+              let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: currentDate)) else {
+            return []
+        }
+
+        return (0..<range.count).compactMap { calendar.date(byAdding: .day, value: $0, to: startOfMonth) }
     }
 }
 
 #Preview {
-    CustomCalendarView()
+    CustomCalendarView(selectedDates: Set<Date>(), onSelectDate: { _ in })
 }
