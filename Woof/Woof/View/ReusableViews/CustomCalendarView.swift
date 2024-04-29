bdcd319bae1e674a59845595d23694ddce875f53
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-04-29 12:57:21 +0200

    Adding picker for a month to display in the calendar.

--- Woof/Woof/View/ReusableViews/CustomCalendarView.swift
+++ Woof/Woof/View/ReusableViews/CustomCalendarView.swift
@@ -5,8 +5,30 @@ struct CustomCalendarView: View {
     let onSelectDate: (Date) -> Void
 
     var body: some View {
+        HStack {
+            Button {
+                let previousMonth = Calendar.current.date(byAdding: .month, value: -1, to: selectedMonth) ?? Date()
+                if !isCurrentMonth(selectedMonth) {
+                    selectedMonth = previousMonth
+                }
+            } label: {
+                Image(systemName: .IconName.chevronLeft)
+            }
+            .opacity(isCurrentMonth(selectedMonth) ? 0 : 1)
+
+            Text(currentMonth)
+
+            Button {
+                selectedMonth = Calendar.current.date(byAdding: .month, value: 1, to: selectedMonth) ?? Date()
+            } label: {
+                Image(systemName: .IconName.chevronRight)
+            }
+        }
+        .font(.title)
+        .padding(.top)
+
         LazyVGrid(columns: Array(repeating: GridItem(), count: calendarColumnsCount), spacing: calendarGridSpacing) {
-            ForEach(getCurrentMonthDates(), id: \.self) { date in
+            ForEach(getCurrentMonthDates(for: selectedMonth), id: \.self) { date in
                 CustomCalendarDayCell(date: date, isSelected: selectedDates.contains(date)) {
                     onSelectDate(date)
                 }
@@ -17,19 +39,30 @@ struct CustomCalendarView: View {
 
     // MARK: - Private interface
 
+    @State private var selectedMonth = Date()
     private let calendarColumnsCount = 7
     private let calendarGridSpacing: CGFloat = 10
+    private var currentMonth: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter.string(from: selectedMonth)
+    }
 
-    private func getCurrentMonthDates() -> [Date] {
+    private func getCurrentMonthDates(for month: Date) -> [Date] {
         let calendar = Calendar.current
-        let currentDate = Date()
-
-        guard let range = calendar.range(of: .day, in: .month, for: currentDate),
-              let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: currentDate)) else {
+        guard let range = calendar.range(of: .day, in: .month, for: month),
+              let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: month)) else {
             return []
         }
 
         return (0..<range.count).compactMap { calendar.date(byAdding: .day, value: $0, to: startOfMonth) }
+    }
+
+    private func isCurrentMonth(_ month: Date) -> Bool {
+        let currentCalendar = Calendar.current
+        let currentComponents = currentCalendar.dateComponents([.year, .month], from: Date())
+        let selectedComponents = currentCalendar.dateComponents([.year, .month], from: month)
+        return selectedComponents.year == currentComponents.year && selectedComponents.month == currentComponents.month
     }
 }
 
