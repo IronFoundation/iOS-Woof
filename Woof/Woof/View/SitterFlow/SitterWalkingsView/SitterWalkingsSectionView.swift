acaa367bd8b7fde9a46591d8f38dac45455fa6b1
NadzeyaShpakouskaya <shpakovna@gmail.com>
2024-04-30 15:09:50 +0400

    Implement `SitterWalkingsSectionView`

--- Woof/Woof/View/SitterFlow/SitterWalkingsView/SitterWalkingsSectionView.swift
+++ Woof/Woof/View/SitterFlow/SitterWalkingsView/SitterWalkingsSectionView.swift
@@ -1,11 +1,41 @@
 import SwiftUI
 
+/// Represents a section of walkings, with an expandable list of walkings.
 struct SitterWalkingsSectionView: View {
+    /// Control the expansion state of the walking list.
+    @Binding var isExpanded: Bool
+
+    /// Walkings that are displayed within this section.
+    let walkings: [Walking]
+
+    /// The title for the section header.
+    let headerTitle: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        DisclosureGroup(
+            isExpanded: $isExpanded,
+            content: {
+                ForEach(walkings) { walking in
+                    NavigationLink {
+                        Text(walking.id.uuidString)
+                    } label: {
+                        SitterWalkingCardView(walking: walking)
+                    }
+                }
+            },
+            label: {
+                Text(headerTitle)
+                    .italic()
+                    .foregroundColor(.App.purpleDark)
+            }
+        )
     }
 }
 
 #Preview {
-    SitterWalkingsSectionView()
+    SitterWalkingsSectionView(
+        isExpanded: .constant(true),
+        walkings: Walking.Dummy.bulkDummyWalkings,
+        headerTitle: "My Walkings"
+    )
 }
