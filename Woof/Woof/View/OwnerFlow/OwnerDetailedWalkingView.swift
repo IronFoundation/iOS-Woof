8da5c32f275a4c188581c847cb25f6cd48ffb678
NadzeyaShpakouskaya <shpakovna@gmail.com>
2024-05-02 09:59:49 +0400

    Make `DetailedWalkingView` as a card

--- Woof/Woof/View/OwnerFlow/OwnerDetailedWalkingView.swift
+++ Woof/Woof/View/OwnerFlow/OwnerDetailedWalkingView.swift
@@ -6,61 +6,100 @@ struct OwnerDetailedWalkingView: View {
     @ObservedObject var viewModel: OwnerDetailedWalkingViewModel
 
     var body: some View {
-        VStack(spacing: AppStyle.UIElementConstant.wideSpacingSize) {
-            HStack {
-                AvatarView(url: viewModel.imageURL)
+        VStack {
+            VStack {
+                HStack {
+                    AvatarView(url: viewModel.imageURL)
 
-                VStack(alignment: .leading, spacing: AppStyle.UIElementConstant.spacingBetweenElements) {
-                    Text(viewModel.fullName)
+                    VStack(
+                        alignment: .leading,
+                        spacing: AppStyle.UIElementConstant.spacingBetweenElements
+                    ) {
+                        Text(viewModel.fullName)
+                            .bold()
+                            .font(
+                                .system(
+                                    size: AppStyle.FontStyle.heading.size
+                                )
+                            )
+                        FiveStarRatingView(stars: viewModel.rating)
+                        Label(
+                            viewModel.phoneNumber,
+                            systemImage: .IconName.phone
+                        )
+                    }
+
+                    Spacer()
+
+                    VStack(spacing: AppStyle.UIElementConstant.spacingBetweenElements) {
+                        Text(
+                            viewModel.price,
+                            format: .currency(code: "USD")
+                        )
                         .bold()
-                        .font(.system(size: AppStyle.FontStyle.heading.size))
-                    FiveStarRatingView(stars: viewModel.rating)
-                        .font(.system(size: AppStyle.FontStyle.footnote.size))
-                    Label(viewModel.phoneNumber, systemImage: .IconName.phone)
+                        WalkingStatusLabel(status: viewModel.status)
+                    }
+                    .font(
+                        .system(
+                            size: AppStyle.FontStyle.heading.size
+                        )
+                    )
                 }
 
-                Spacer()
+                Divider()
 
-                VStack(spacing: AppStyle.UIElementConstant.spacingBetweenElements) {
-                    WalkingStatusLabel(status: viewModel.status)
-                    Text(
-                        viewModel.price,
-                        format: .currency(code: "USD")
+                VStack(
+                    alignment: .leading,
+                    spacing: AppStyle.UIElementConstant.minPadding
+                ) {
+                    HStack {
+                        Label(
+                            timeLabel,
+                            systemImage: .IconName.clock
+                        )
+                        Text(viewModel.time)
+                            .bold()
+                    }
+                    Divider()
+                    HStack {
+                        Label(
+                            dateLabel,
+                            systemImage: .IconName.calendar
+                        )
+                        Text(viewModel.date)
+                            .bold()
+                    }
+                    Divider()
+                    Label(
+                        addressLabel,
+                        systemImage: .IconName.house
                     )
-                    .bold()
-                    .font(.system(size: AppStyle.FontStyle.heading.size))
+                    Text(viewModel.address)
+                        .bold()
+                    Divider()
+                    Label(
+                        notesLabel,
+                        systemImage: .IconName.notes
+                    )
+                    Text(viewModel.notes)
+                        .bold()
                 }
             }
-            Text(header)
-                .bold()
-                .italic()
-                .font(.system(size: AppStyle.FontStyle.heading.size))
-            VStack(alignment: .leading, spacing: AppStyle.UIElementConstant.minPadding) {
-                HStack {
-                    Label(timeLabel, systemImage: .IconName.clock)
-                    Text(viewModel.time).bold()
-                }
-                Divider()
-                HStack {
-                    Label(dateLabel, systemImage: .IconName.calendar)
-                    Text(viewModel.date).bold()
-                }
-                Divider()
-                Label(addressLabel, systemImage: .IconName.house)
-                Text(viewModel.address)
-                    .bold()
-                Divider()
-                Label(notesLabel, systemImage: .IconName.notes)
-                Text(viewModel.notes)
-                    .bold()
-            }.padding()
-            .background(Color.App.grayLight)
-                .clipShape(
-                    RoundedRectangle(cornerRadius: AppStyle.UIElementConstant.cornerRadius)
+            .padding()
+            .font(
+                .system(
+                    size: AppStyle.FontStyle.footnote.size
                 )
+            )
+            .background(Color.App.grayLight)
+            .clipShape(
+                RoundedRectangle(
+                    cornerRadius: AppStyle.UIElementConstant.cornerRadius
+                )
+            )
             Spacer()
-        }.padding()
-        
+        }
+        .padding()
     }
 
     // MARK: - Private interface
