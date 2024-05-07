5e9860b6dce4eba8ce96126d925c218987dc5caa
kirpichonok <ivankirpichonok@gmail.com>
2024-05-07 15:29:49 +0300

    Update script build-ios-simulator: remove NetworkService building, change configuration on debug.

--- scripts/build-ios-simulator.sh
+++ scripts/build-ios-simulator.sh
@@ -4,8 +4,6 @@ readonly APP_NAME="Woof"
 readonly IOS_SDK="17.0"
 
 xcodebuild -project ${APP_NAME}/${APP_NAME}.xcodeproj \
-           -scheme NetworkService
-xcodebuild -project ${APP_NAME}/${APP_NAME}.xcodeproj \
            -scheme ${APP_NAME} \
-           -configuration Release \
-           -sdk iphonesimulator${IOS_SDK}
+           -sdk iphonesimulator${IOS_SDK} \
+           -quiet
