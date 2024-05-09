3919c1ce1fc7e52fb50184e87d77619fef9476a6
kirpichonok <ivankirpichonok@gmail.com>
2024-05-09 16:36:17 +0300

    Update workflow: extract xcpretty setup as a separate step.

--- scripts/build-ios-simulator.sh
+++ scripts/build-ios-simulator.sh
@@ -3,9 +3,9 @@
 readonly APP_NAME="Woof"
 readonly IOS_SDK="17.0"
 
-gem install xcpretty
-export PATH="$(gem environment gemdir)/bin:$PATH"
-xcodebuild -project ${APP_NAME}/${APP_NAME}.xcodeproj \
-           -scheme ${APP_NAME} \
-           -sdk iphonesimulator${IOS_SDK} \
-           | xcpretty
+source ~/.bash_profile
+cd ${APP_NAME}
+set -o pipefail && xcodebuild \
+-scheme ${APP_NAME} \
+-sdk iphonesimulator${IOS_SDK} \
+| xcpretty
--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -4,10 +4,10 @@ readonly TEST_SCHEME="WoofTests"
 readonly DESTINATION_PLATFORM="iOS Simulator"
 readonly DESTINATION_NAME="iPhone 15 Pro"
 
+source ~/.bash_profile
 cd Woof
-xcodebuild \
-  -scheme "${TEST_SCHEME}" \
-  -sdk iphoneos \
-  -destination "platform=${DESTINATION_PLATFORM},name=${DESTINATION_NAME}" \
-  -quiet \
-  test
+set -o pipefail && xcodebuild test \
+-scheme "${TEST_SCHEME}" \
+-sdk iphoneos \
+-destination "platform=${DESTINATION_PLATFORM},name=${DESTINATION_NAME}" \
+| xcpretty
