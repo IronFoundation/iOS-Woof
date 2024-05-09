f2ff08a141838c59dc64271a841b46756bf206d5
kirpichonok <ivankirpichonok@gmail.com>
2024-05-09 14:20:51 +0300

    Update build-and-test-app script: add xcpretty to improve build output.

--- scripts/build-ios-simulator.sh
+++ scripts/build-ios-simulator.sh
@@ -3,7 +3,9 @@
 readonly APP_NAME="Woof"
 readonly IOS_SDK="17.0"
 
+gem install xcpretty
+export PATH="$(gem environment gemdir)/bin:$PATH"
 xcodebuild -project ${APP_NAME}/${APP_NAME}.xcodeproj \
            -scheme ${APP_NAME} \
            -sdk iphonesimulator${IOS_SDK} \
-           -quiet
+           | xcpretty
